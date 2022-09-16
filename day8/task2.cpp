1509719dbfe2798793564c62790807fb36285319
Agaton Fransson <agfrxa@syntronic.com>
2022-09-16 15:46:14 +0200

    Finish day8 task2

---
+++ day8/task2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+int main(){
+
+    std::fstream instruction_stream;
+    std::string instruction;
+    std::vector<std::string> instructions;
+    instruction_stream.open("C:/Users/agfrxa/Python/advent of code 2020/day8/input.txt", std::ios::in);
+//    rules.open("D:/Python/Advent-of-code-2020-python/day7/input.txt", std::ios::in);
+    while (std::getline(instruction_stream, instruction)){
+        instructions.push_back(instruction);
+    }
+    int len = instructions.size();
+
+    for (int i=0 ; i < len ; i++){
+        std::string temp_instruction;
+        int current_index = 0;
+        int current_value = 0;
+        int iter = 0;
+        int status = 0;
+        std::vector<std::string> copy_instructions = instructions;
+
+        if( copy_instructions[i].substr(0, 3) == "jmp" ){
+            copy_instructions[i].replace(0,3, "nop");
+            status ++;
+        }
+        if (( copy_instructions[i].substr(0, 3) == "nop" ) && ( status == 0 )){
+            copy_instructions[i].replace(0,3, "jmp");
+        }
+
+        while (copy_instructions[current_index] != "used"){
+            iter ++;
+            if ( iter > 10000 ){
+                break;
+            }
+            temp_instruction = copy_instructions[current_index];
+            copy_instructions[current_index] = "used";
+            if ( temp_instruction.substr(0, 3) == "jmp" ){
+                if ( temp_instruction.substr(4, 1) == "+" ){
+                    current_index += std::stoi(temp_instruction.substr(5, std::string::npos));
+                }
+                else{
+                    current_index -= std::stoi(temp_instruction.substr(5, std::string::npos));
+                }
+            }
+
+            else if ( temp_instruction.substr(0, 3) == "acc" ){
+                current_index ++;
+                if ( temp_instruction.substr(4, 1) == "+" ){
+                    current_value += std::stoi(temp_instruction.substr(5, std::string::npos));
+                }
+                else{
+                    current_value -= std::stoi(temp_instruction.substr(5, std::string::npos));
+                }
+            }
+            else {
+                current_index ++;
+            }
+            if (copy_instructions.back() == "used") {
+                std::cout << current_value;
+                break;
+            }
+        }
+        }
+}
